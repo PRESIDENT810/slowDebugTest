46bc91b421099624098cd9d1661c8d39f5ebbc17
zhongkaining.paxos <zhongkaining.paxos@bytedance.com>
2022-02-14 10:34:03 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# [WIP] sloweDebugTest
+
+This project aims to reproduce a case that it's very slow to debug iOS project with a huge number of static frameworks in Xcode. Still workign in progress.
--- slowDebugTest/ViewController.swift
+++ slowDebugTest/ViewController.swift
@@ -6,14 +6,23 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    lazy var box = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        sleep(10) // wait so I have time to let debugger attach to process lldb-rpc-server
+        
+        self.view.addSubview(box)
+        box.backgroundColor = .green
+        box.snp.makeConstraints { (make) -> Void in
+           make.width.height.equalTo(50)
+           make.center.equalTo(self.view)
+        }
     }
-
-
 }
 
